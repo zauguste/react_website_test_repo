7e65280d98db069cd3621d3707f927d037f22ed2
unknown <zauguste52@gmail.com>
2025-04-04 16:10:27 -0400

    button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
   // now import header compnent you want to render
-import Card from './Card.jsx'
+import Button from './Button.jsx'
 // we use return to return our html like code 
 function App() {
   return(
     <>
-      <Card></Card>
-      <Card></Card>
+      <Button/>
+
     </>
     
   );
